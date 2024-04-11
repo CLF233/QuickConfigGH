1f97ba15bc261a8973ee4fe90c45869405300af0
CLF <125818668+CLF233@users.noreply.github.com>
2024-04-11 22:59:21 +0800

    a

--- GHAutoConfig.sh
+++ GHAutoConfig.sh
@@ -6,9 +6,9 @@ set -x
 #配置默认初始仓库分支为main
 git config --global init.defaultBranch "main"
 #配置username(可改为你自己的GitHub用户名)
-git config --global user.name "Atopes Sayuri"
+git config --global user.name "$1"
 #配置用户邮箱(可改为你自己注册GitHub邮箱)
-git config --global user.email "108107785+AtopesSayuri@users.noreply.github.com"
+git config --global user.email "$2"
 #配置最大缓冲区大小为10G(防止推送/拉取失败)
 git config --global http.postBuffer 10737418240
 git config --global https.postBuffer 10737418240
